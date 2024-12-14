4731c79584610b38c70b02f5fd44c7b9ed517781
YasBroek <yasmin.van-den-broek@ensta-paris.fr>
2024-12-14 20:06:24 +0100

    Update class_bloc.h

--- class_bloc.h
+++ class_bloc.h
@@ -3,25 +3,24 @@
 
 #include <iostream>
 #include <array>
+#include QColor
 
 class Bloc {
 protected:
-    std::array<std::array<int, 4>, 4> forme; // Array 2D
-    int direction;  // Direção atual da peça
-    int color;      // Cor da peça
+    std::array<std::array<int, 4>, 4> forme;
+    QColor color;      // Cor da peça
 
 public:
-    Bloc(int c) : color(c), direction(0), orientation(0) {
+    Bloc(QColor c) : color(c), orientation(0) {
     forme = {{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}};
 };
-
-    // Métodos
-    void rotation90();
+    void rotation90_clockwise();
+    void rotation90_counterclockwise();
     const std::array<std::array<int, 4>, 4>& getForme() const; 
     virtual ~Bloc() = default;
 };
 
-void Bloc::rotation90() {
+void Bloc::rotation90_clockwise() {
     std::array<std::array<int, 4>, 4> temp = forme;
     int n = forme.size();
 
@@ -30,8 +29,19 @@ void Bloc::rotation90() {
             forme[j][n - 1 - i] = temp[i][j];
         }
     }
-    direction = (direction + 1) % 4; 
 }
+
+void Bloc::rotation90_counterclockwise() {
+    std::array<std::array<int, 4>, 4> temp = forme;
+    int n = forme.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < forme[i].size(); j++) {
+            forme[n - 1 - j][i] = temp[i][j];
+        }
+    }
+}
+
 
 const std::array<std::array<int, 4>, 4>& Bloc::getForme() const {
     return forme;
