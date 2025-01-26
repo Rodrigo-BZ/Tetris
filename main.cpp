7139b9d95ff68f7cc615f4fd1dfb192ca55dcfe0
Rodrigo Botelho Zuiani <rodrigo.botelho@ensta-paris.fr>
2025-01-26 20:11:02 +0100

    Fix dark theme problems

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ int main(int argc, char *argv[])
 {
     qputenv("Qt_QPA_PLATFORM", "windows:darkmode=2");
     QApplication a(argc, argv);
-    a.setStyle("modern");
 
     MenuWindow menuWindow;
     GameWindow gameWindow;
+
+    gameWindow.setObjectName("GameWindow");
+    gameWindow.setStyleSheet("QWidget#GameWindow{background-color: #1e1e1e}");
+
     menuWindow.GetGameWindowPtr(&gameWindow);
     gameWindow.GetMenuWindowPtr(&menuWindow);
     menuWindow.show();
